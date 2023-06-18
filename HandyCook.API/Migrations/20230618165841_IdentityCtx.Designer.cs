05473e9c3d1c6fa020bb89a865be337feee6045f
Oskar Bzdoń <bzdon.oskar@gmail.com>
2023-06-18 19:39:27 +0200

    created login/register endpoints

--- HandyCook.API/Migrations/20230618165841_IdentityCtx.Designer.cs
+++ HandyCook.API/Migrations/20230618165841_IdentityCtx.Designer.cs
@@ -4,6 +4,7 @@ using HandyCook.API;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HandyCook.API.Migrations
 {
     [DbContext(typeof(HCContext))]
-    partial class HCContextModelSnapshot : ModelSnapshot
+    [Migration("20230618165841_IdentityCtx")]
+    partial class IdentityCtx
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

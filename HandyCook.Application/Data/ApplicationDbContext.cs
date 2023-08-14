9b240c1fb8674d8c169eb1b041a9baf718d63f65
Oskar Bzdoń <bzdon.oskar@gmail.com>
2023-08-14 18:21:24 +0200

    project reconstructed

---
+++ HandyCook.Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HandyCook.Application.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ HandyCook.Application/Data/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandyCook.Application.Data
+{
+    public class User : IdentityUser
+    {
+        [InverseProperty(nameof(Recipe.UserNavigation))]
+        public virtual ICollection<Recipe> Recipes { get; set; }
+    }
+}

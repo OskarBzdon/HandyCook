62497488080e2128e3eac8838713a165bfbc361d
Oskar Bzdoń <bzdon.oskar@gmail.com>
2023-09-26 20:53:01 +0200

    displaying raw data about recipes in index

---
+++ HandyCook.Application/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using HandyCook.Application.Data;
+
+namespace HandyCook.Application.Pages
+{
+    public partial class Index
+    {
+        public IEnumerable<Recipe> Recipes { get; set; } = new HashSet<Recipe>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (Recipes is null || Recipes.Count() is 0)
+            {
+                Recipes = ctx.Recipes;
+            }
+
+            await base.OnInitializedAsync();
+        }
+    }
+}

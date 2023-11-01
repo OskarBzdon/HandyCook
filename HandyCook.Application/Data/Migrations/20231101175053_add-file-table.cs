40beb47cd659e84d5ecbc6c0a29d4b0cb3e7ba7f
Oskar Bzdoń <bzdon.oskar@gmail.com>
2023-11-01 18:51:33 +0100

    added image table

---
+++ HandyCook.Application/Data/Migrations/20231101175053_add-file-table.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HandyCook.Application.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfiletable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "File",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bytes = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Format = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_File", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_File_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_File_RecipeId",
+                table: "File",
+                column: "RecipeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "File");
+        }
+    }
+}
--- HandyCook.Application/Data/Recipe.cs
+++ HandyCook.Application/Data/Recipe.cs
@@ -11,6 +11,8 @@ namespace HandyCook.Application.Data
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public virtual ICollection<File> Images { get; set; }
+
         [ForeignKey(nameof(Recipe.UserNavigation))]
         public string UserNavigationId { get; set; }
         public virtual User UserNavigation { get; set; }
